d60178112629bb19e4bbd3d3f5e47613f107e178
unknown <dananad@126.com>
2017-01-10 21:03:42 +0800

    daily commit

--- getCharsInOnePhoto.js
+++ getCharsInOnePhoto.js
@@ -1,7 +1,14 @@
 const Jimp = require('jimp')
 const util = require('util')
+
 const imageDir = './test/resource/imagelibrary/imagelibrary/'
 let imagePath = imageDir + 'big_ccp_ALL1_nodiff_nzd.png'
+
+//存放某多组像素，每组像素都组成一个汉字
+let allCharsPixel = []
+
+//存放已访问过的像素点的数组
+let pixelsVisited = {}
 
 Jimp.read(imagePath)
   .then(function (image) {
@@ -12,7 +19,14 @@ Jimp.read(imagePath)
       for (let x = 0; x < width; x++) {
 
         let pixelColor = image.getPixelColor(x, y)
-        console.log(Jimp.intToRGBA(pixelColor))
+        let pixel = {
+          x: x,
+          y: y,
+          color: Jimp.intToRGBA(pixelColor)
+        }
+        visitPixelBeside(pixel)
+        if (isBlack(pixel))
+          console.log(util.inspect(pixel))
       }
     }
   })
@@ -20,18 +34,18 @@ Jimp.read(imagePath)
     console.log(util.inspect(err))
   })
 
+let isBlack = function (pixel) {
+  if (pixel && pixel.color) {
+    return (pixel.color.r === 0 && pixel.color.g === 0 && pixel.color.b === 0)
+  }
+  return false
+}
 
-// //验证码图片中的所有像素点
-// var allPixelsInPhoto = []
-//   //存放某一个中文字像素点的数组
-// var allPixelsInOneChar = []
-//   //存放已访问过的像素点的数组
-// var pixelsVisited = []
 
-
-// for (pixel in allPixelsInPhoto) {
-//   visitPixelBeside(pixel)
-// }
+let isPixelVisit = function (pixel, pixelsVisited) {
+  let positionString = pixel.x + ',' + pixel.y
+  return pixelsVisited[positionString]
+}
 
 /**
  *   判断一个像素点是否是黑色像素，如果是黑色像素，则把这个像素点加到allPixelsInOneChar
@@ -43,15 +57,15 @@ Jimp.read(imagePath)
  * @param  {[type]} allPixelsInOneChar [某一个字中的像素点]
  * @param  {[type]} allPixelsInPhoto   [图片中的所有像素点]
  */
-function visitPixelBeside(pixel, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto) {
-  if (isPixelInPhoto(pixel, allPixelsInPhoto)) {
-    return
-  }
-  pixelsVisited.push(pixel)
+function visitPixelBeside(pixel, pixelsVisited, allPixelsInOneChar, image) {
+  let allPixelsInOneChar = []
   if (isPixelVisit(pixel, pixelsVisited)) {
     return
   }
-  if (isPixelWhite(pixel)) {
+  let positionString = pixel.x + ',' + pixel.y
+  pixelsVisited[positionString] = true
+
+  if (!isPixelBlack(pixel)) {
     return
   }
   allPixelsInOneChar.push(pixel)
@@ -67,27 +81,29 @@ function visitPixelBeside(pixel, pixelsVisited, allPixelsInOneChar, allPixelsInP
   //
   // //↙
   // visitPixelLeftDown(pixel, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
+
+  return allPixelsInOneChar
 }
 
-// function visitPixelRight(pixel, allPixelsInPhoto) {
-//   var pixelToVisit = getRigthPixel(pixel, allPixelsInPhoto)
-//   visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
-// }
-//
-// function visitPixelRightDown(pixel, allPixelsInPhoto) {
-//   var pixelToVisit = getRightDownPixel(pixel, allPixelsInPhoto)
-//   visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
-// }
-//
-// function visitPixelDown(pixel) {
-//   var pixelToVisit = getDownPixel(pixel, allPixelsInPhoto)
-//   visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
-// }
-//
-// function visitPixelLeftDown(pixel) {
-//   var pixelToVisit = getLeftDownPixel(pixel, allPixelsInPhoto)
-//   visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
-// }
+function visitPixelRight(pixel, image) {
+  var pixelToVisit = getRigthPixel(pixel, allPixelsInPhoto)
+  visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
+}
+
+function visitPixelRightDown(pixel, allPixelsInPhoto) {
+  var pixelToVisit = getRightDownPixel(pixel, allPixelsInPhoto)
+  visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
+}
+
+function visitPixelDown(pixel) {
+  var pixelToVisit = getDownPixel(pixel, allPixelsInPhoto)
+  visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
+}
+
+function visitPixelLeftDown(pixel) {
+  var pixelToVisit = getLeftDownPixel(pixel, allPixelsInPhoto)
+  visitPixelBeside(pixelToVisit, pixelsVisited, allPixelsInOneChar, allPixelsInPhoto)
+}
 
 
 function pasteCharOnNewPhoto(allPixelsInOneChar, newPhoto) {
